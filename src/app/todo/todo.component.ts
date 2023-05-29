e06958714395cb135747a9c1b6ab67a362860b19
Bnop1146 <79976803+Bnop1146@users.noreply.github.com>
2023-05-29 18:40:14 +0200

    Sidste ændringer på denne

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -9,46 +8,43 @@ import { FormBuilder } from '@angular/forms';
 })
 export class TodoComponent implements OnInit {
   completed: boolean = false;
-  taskList: any[] = []
-  newTodoForm = this.formBuilder.group({
-    todoItem: ''
-  })
+  taskList: any[] = [];
+  newTodoForm: FormGroup; // Declare newTodoForm as a FormGroup
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.newTodoForm = this.formBuilder.group({
+      todoItem: '',
+      description: ''
+    });
     
-  constructor(
-    private formBuilder: FormBuilder
-  ) { }
-  
-  
+    this.taskList = window.localStorage.getItem('task') ? JSON.parse(localStorage.getItem('task')) : [];
+  }
 
   addTask() {
-    const value = this.newTodoForm.value.todoItem
-    this.taskList.push({ id: this.taskList.length, name: value })
-    window.localStorage.setItem('task', JSON.stringify(this.taskList))
+    const name = this.newTodoForm.value.todoItem;
+    const description = this.newTodoForm.value.description;
+    this.taskList.push({ id: this.taskList.length, name: name, description: description });
+    window.localStorage.setItem('task', JSON.stringify(this.taskList));
     this.newTodoForm.reset();
-    
   }
 
   removeTask(i: any) {
-    this.taskList.splice(i, 1)
-    window.localStorage.setItem('task', JSON.stringify(this.taskList))
-    
+    this.taskList.splice(i, 1);
+    window.localStorage.setItem('task', JSON.stringify(this.taskList));
   }
 
-  markDone(value: any) {
-    value.completed = !value.completed
-    value.completed === true ?
-      this.taskList.push(this.taskList.splice(this.taskList.indexOf(value), 1)[0]) :
-      this.taskList.unshift(this.taskList.splice(this.taskList.indexOf(value), 1)[0])
-  }
-  
-  
-
-  ngOnInit(): void {
-    this.taskList = window.localStorage.getItem('task') ? JSON.parse(localStorage.getItem('task')) : []
+  editTask(task: any) {
+    task.editMode = true;
   }
 
+  saveTask(task: any) {
+    task.editMode = false;
+    window.localStorage.setItem('task', JSON.stringify(this.taskList));
+  }
+
+  cancelEdit(task: any) {
+    task.editMode = false;
+  }
 }
-function todoItem(todoItem: any) {
-  throw new Error('Function not implemented.');
-}
-
